05a795fe9ea4b7502db5eab161350117b1182c2a
stinki <taryous-03@hotmail.fr>
2024-12-01 09:44:55 +0100

    part 1 is done

--- d_client.py
+++ d_client.py
@@ -7,11 +7,11 @@ contador=0;
 
 s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while True:
-    data = input(" > ").encode(encoding="utf-8")
-    #data = t.time()
-    #data = f"{contador} + {t.time()}"
+    stime = t.time()
+    data = f"{contador} + {stime}"
     data = str(data).encode(encoding="utf-8")
     s.sendto(data,(host,port))
-    #contador += 1
+    contador += 1
     data,addr = s.recvfrom(1024)
-   
+    RTT = t.time() - stime
+    print(f" [+] RTT : {RTT}")
--- d_server.py
+++ d_server.py
@@ -1,12 +1,12 @@
 import socket
 import time as t
 host = 'localhost'
-port = 10000
+port = 20001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((host,port))
 while True:
     data,addr = s.recvfrom(1024)
-    stime = str(s.time())
+    stime = str(t.time())
     print(f" Connected by {addr} > {data.decode('utf-8')}")
     s.sendto(stime.encode(encoding='utf-8'),addr)
